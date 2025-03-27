dc0698921f22cc5ef3f2d31d7b1d8cfe4a4e6641
Ishan Kanaujiya <ishankanaujiya96@gmail.com>
2025-03-27 20:24:01 +0545

    Layout For Displaying Added Folder and File Made

--- lib/MainHome.dart
+++ lib/MainHome.dart
@@ -19,6 +19,48 @@ class _HomeState extends State<Home> {
 
         ],
       ),
+
+        body: Padding(
+          padding: EdgeInsets.only(top: 20, left: 10, right: 10),
+          child: SingleChildScrollView(
+            child: Wrap(
+              spacing: 10, // Space between items
+              runSpacing: 10, // Space between rows when wrapped
+              children: List.generate(
+                60, // Number of items
+                    (index) => Container(
+                  width: 180,
+                      height: 200,// Fixed width for proper wrapping
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.blue.shade100,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                     Row(
+                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                       children: <Widget>[
+
+                         Text("Title $index", style: TextStyle(fontWeight: FontWeight.bold)),
+                         Icon(Icons.menu),
+                       ],
+                     ),
+
+                      Text("Date"),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text("Add"),
+      ),
     );
   }
 }
