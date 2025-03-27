966ad877094dc5f9bbe0e2366bb26554ff8ccf98
Ishan Kanaujiya <ishankanaujiya96@gmail.com>
2025-03-27 19:50:28 +0545

    Initial Commit

---
+++ lib/MainHome.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+class Home extends StatefulWidget {
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text("Welcome"),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.menu)
+          ),
+
+        ],
+      ),
+    );
+  }
+}
