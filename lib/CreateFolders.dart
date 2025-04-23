d5820702f433d6bb8b605b9f96046d404cbd49b1
Ishan Kanaujiya <ishankanaujiya96@gmail.com>
2025-04-23 12:00:36 +0545

    New Page for Folder Creation Added

--- lib/CreateFolders.dart
+++ lib/CreateFolders.dart
@@ -5,6 +5,8 @@ class CreateNewFolders extends StatefulWidget {
 }
 
 class _CreateNewFoldersState extends State<CreateNewFolders> {
+  List<String> folderName = [];
+  bool addFolderClickCheck = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,7 +16,69 @@ class _CreateNewFoldersState extends State<CreateNewFolders> {
         leading: IconButton(onPressed: () {}, icon: Icon(Icons.keyboard_backspace_rounded)),
       ),
 
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            addFolderClickCheck ? FolderForm() :
+            SizedBox(
+              width: double.infinity,
+                child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        addFolderClickCheck = true;
+                      });
+
+                    },
+                    child: Text("Add Folders"),
+                ),
+            ),
+          ],
+        ),
+      ),
 
     );
   }
 }
+
+class FolderForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      //crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            width: 400,
+            height: 400,
+            color: Colors.cyan,
+            child: Column(
+              children: [
+                Container(
+                  width: 250,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      label: Text("Folder Name"),
+                    ),
+                  ),
+                ),
+
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(onPressed: () {}, child: Text("Done")),
+              ],
+            ),
+
+
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+
