29d8073e63ff183192881d94b2bfdc087a0d0eb0
Ishan Kanaujiya <ishankanaujiya96@gmail.com>
2025-04-23 15:14:00 +0545

    Folder Created Dynamically

--- lib/CreateFolders.dart
+++ lib/CreateFolders.dart
@@ -5,8 +5,12 @@ class CreateNewFolders extends StatefulWidget {
 }
 
 class _CreateNewFoldersState extends State<CreateNewFolders> {
-  List<String> folderName = [];
+  List<String> folderName = ["values"];
+  List<String> folderNameHolder = [];
   bool addFolderClickCheck = false;
+
+
+  var folderNameInput = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,69 +20,112 @@ class _CreateNewFoldersState extends State<CreateNewFolders> {
         leading: IconButton(onPressed: () {}, icon: Icon(Icons.keyboard_backspace_rounded)),
       ),
 
-      body: Container(
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            addFolderClickCheck ? FolderForm() :
-            SizedBox(
-              width: double.infinity,
-                child: ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        addFolderClickCheck = true;
-                      });
+      body: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+        
+              // Container(
+              //   height: 400,
+              //   color: Colors.cyan,
+              //   child: ListView.builder(itemBuilder: (context,index) {
+              //     return Container(
+              //       child: Text("${folderName[index]}"),
+              //     );
+              //   },
+              //     itemCount: folderName.length,
+              //   ),
+              // ),
 
-                    },
-                    child: Text("Add Folders"),
+
+              SizedBox(
+                height: 40,
+              ),
+              for(var i = 0; i<folderName.length; i++)
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10.0, left: 20.0),
+                  child: Container(
+                    width: double.infinity,
+
+                    child: Text("${folderName[i]}", style: TextStyle(
+                      fontSize: 40,
+
+                    ),),
+                  ),
                 ),
-            ),
-          ],
+        
+              addFolderClickCheck ? Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 400,
+        
+        
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: double.infinity,
+                        child: TextField(
+                          controller: folderNameInput,
+                          decoration: InputDecoration(
+                            label: Text("Folder Name"),
+                          ),
+                        ),
+                      ),
+        
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 200,
+                            height: 50,
+                            child: ElevatedButton(
+                                onPressed: () {
+                                  folderName.add(folderNameInput.text);
+                                  setState(() {
+                                    addFolderClickCheck = false;
+                                    folderNameInput.clear();
+                                  });
+
+                                },
+                                child: Text("Done"),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+        
+        
+                ),
+              ) :
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0, bottom: 40.0),
+                child: SizedBox(
+                  width: double.infinity,
+                    child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            addFolderClickCheck = true;
+                          });
+
+                        },
+                        child: Text("Add Folders"),
+                    ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
 
     );
   }
 }
-
-class FolderForm extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      //crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            width: 400,
-            height: 400,
-
-            child: Column(
-              children: [
-                Container(
-                  width: 250,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      label: Text("Folder Name"),
-                    ),
-                  ),
-                ),
-
-                SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(onPressed: () {}, child: Text("Done")),
-              ],
-            ),
-
-
-          ),
-        ),
-      ],
-    );
-  }
-}
-
 
